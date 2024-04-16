d5a8d7220f3252f872627c32d89599a841e6bda0
princeraval147 <princeraval147@gmail.com>
2024-04-16 13:28:56 +0530

    Project_03

---
+++ TailwindProps/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Prince",
+    age: 19
+  }
+  let newArr = [1, 2, 3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="ReactApp" btnText="click me" />
+      <Card username="Prince" />
+    </>
+  )
+}
+
+export default App
