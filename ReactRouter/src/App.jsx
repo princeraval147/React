853b10a099e0e5cafc73b0a9a570623af9d8ee9d
princeraval147 <princeraval147@gmail.com>
2024-05-01 18:44:20 +0530

    Project_07

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './App.css'
+import React from "react"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from "react-router-dom"
 
 const App = () => {
   return (
     <>
-      <h1 className='bg-green-600 p-4'>React Router</h1>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
