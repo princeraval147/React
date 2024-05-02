21d0e3dc6046754ff1f19325c23f5ae6d399f5b4
princeraval147 <princeraval147@gmail.com>
2024-05-02 12:07:57 +0530

    Project_07

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react"
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { Outlet } from "react-router-dom"
+import { Outlet, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
 
 const App = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
+      {/* <Outlet /> */}
       <Footer />
     </>
   )
