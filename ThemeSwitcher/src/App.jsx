fa794d709177dbff4b580fbbe167106d112ff445
princeraval147 <princeraval147@gmail.com>
2024-05-08 11:34:30 +0530

    Project_09

---
+++ ThemeSwitcher/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <h1 className='p-4 bg-pink-600 text-3xl'>Hello</h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* theme */}
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            {/* card */}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
