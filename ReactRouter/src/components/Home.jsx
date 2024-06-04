8d887eb158493e7627de7feb289abc8a4b8f830d
princeraval147 <princeraval147@gmail.com>
2024-06-04 16:59:54 +0530

    Project_07

--- ReactRouter/src/components/Home.jsx
+++ ReactRouter/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+    const userName = JSON.parse(localStorage.getItem('user'));
+
     return (
         <>
-            <div className="mx-auto w-full max-w-7xl">
+            <div id='Home' className="mx-auto w-full max-w-7xl">
                 <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
                     <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                         <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
@@ -13,9 +15,9 @@ const Home = () => {
                                 <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
                             </h2>
 
-                            <Link
+                            {/* <Link
                                 className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
-                                to="/"
+                                to="/signUp"
                             >
                                 <svg
                                     fill="white"
@@ -28,7 +30,10 @@ const Home = () => {
                                     <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
                                 </svg>
                                 &nbsp; Download now
-                            </Link>
+                            </Link> */}
+                            {
+                                localStorage.getItem('user') === null ? <h1>Welcome To Our Website</h1> : <h1>welcome {userName.Name}</h1>
+                            }
                         </div>
                     </div>
 
