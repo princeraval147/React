5a5c3774e759d055bcd908931a344c65bd1fd221
princeraval147 <princeraval147@gmail.com>
2024-06-26 22:10:02 +0530

    Redux Toolkit

--- ReduxToolkitTodo/src/App.jsx
+++ ReduxToolkitTodo/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
 
   return (
     <>
       <h1>Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
